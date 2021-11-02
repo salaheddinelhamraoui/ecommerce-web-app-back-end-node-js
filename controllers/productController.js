faa5a7e2665a959494ef2eaf1f68717ac7eb446d
salaheddinelhamraoui <salah.infospace@gmail.com>
2021-11-02 23:54:37 +0100

    add show product controller

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,5 +1,7 @@
 const Product = require('../models/product');
 
+const Joi = require('joi');
+
 const fs = require('fs');
 
 const formidable = require('formidable');
@@ -23,11 +25,33 @@ exports.createProduct = (req, res) => {
 
         if (files.photo) {
 
+            if (files.photo.size > Math.pow(10, 6)) {
+                return res.status(400).json({
+                    error: 'Image should be less than 1mb in size !'
+                })
+            }
+
             product.photo.data = fs.readFileSync(files.photo.filepath)
             product.photo.contentType = files.photo.mimetype
         }
 
 
+
+        const schema = Joi.object({
+            name: Joi.string().required(),
+            description: Joi.string().required(),
+            price: Joi.required(),
+            quantity: Joi.required(),
+            category: Joi.required()
+        })
+
+        const { error } = schema.validate(fields);
+
+        if (error) {
+            return res.status(400).json({
+                error: error.details[0].message
+            })
+        }
 
 
         product.save((err, product) => {
@@ -47,4 +71,40 @@ exports.createProduct = (req, res) => {
     })
 
 
+}
+
+exports.productById = (req, res) => {
+    Product.findById(id).exec((err, product) => {
+
+        if (err || !product) {
+            return res.status(400).json({
+                error: 'Product not found !'
+            })
+        }
+
+        req.product = product
+
+        next()
+    })
+}
+
+exports.showProduct = (req, res) => {
+
+    req.product.photo = undefined;
+
+    res.json ({
+        product: req.product
+    })
+}
+
+exports.removeProduct = (req, res) {
+
+    let product = req.product
+
+    product.remove((err, product) => {
+
+        if(err) {
+            
+        }
+    })
 }
--- routes/products.js
+++ routes/products.js
@@ -4,12 +4,17 @@ const { userById } = require('../middlewares/user');
 
 const router = express.Router();
 
-const { createProduct } = require('../controllers/productController');
+const { createProduct, showProduct, productById, removeProduct } = require('../controllers/productController');
 
 const { requireSignIn, isAuth, isAdmin } = require('../middlewares/auth');
 
+router.post('/:productId', showProduct)
+
 router.post('/create/:userId', [requireSignIn, isAuth, isAdmin], createProduct);
 
+router.delete('/:productId', [requireSignIn, isAuth, isAdmin], createProduct);
+
 router.param('userId', userById);
+router.param('productId', productById)
 
 module.exports = router;
